d11f48ab3819de4fc8da7e63ffbf0345e520d37d
marcellaandries <marcella.andries11@gmail.com>
2023-07-14 02:22:14 +0700

    logout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -80,6 +80,13 @@
                                                     <li class="menu-item" >
                                                         <a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
                                                     </li>
+                                                    <li class="menu-item" >
+                                                        {{-- closest('form').submit(); --}}
+                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id ="logout-form" method="POST" action="{{ route('logout') }}">
+                                                        @csrf
+                                                    </form>
                                                 </ul>
                                             </li>
                                         @else
@@ -89,6 +96,13 @@
                                                     <li class="menu-item" >
                                                         <a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
                                                     </li>
+                                                    <li class="menu-item" >
+                                                        {{-- closest('form').submit(); --}}
+                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                                    </li>
+                                                    <form id ="logout-form" method="POST" action="{{ route('logout') }}">
+                                                        @csrf
+                                                    </form>
                                                 </ul>
                                             </li>
 

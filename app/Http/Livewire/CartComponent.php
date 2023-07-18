7d26d5c443b688e7f15f4d5908df95d0998c2ed1
marcellaandries <marcella.andries11@gmail.com>
2023-07-18 13:20:39 +0700

    inc dec qty

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
@@ -12,9 +13,21 @@ class CartComponent extends Component
     }
 
     public function rupiah($var_number){
-
         $rupiah_result = "Rp " . number_format($var_number,2,',','.');
         return $rupiah_result;
+    }
 
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
     }
 }

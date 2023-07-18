ce5b5a6a287a1588353af5a50c25a4d3ddc3d033
marcellaandries <marcella.andries11@gmail.com>
2023-07-18 11:53:39 +0700

    msg

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -12,10 +12,10 @@ class ShopComponent extends Component
     public function store($product_id,$product_name,$product_price)
     {
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
-        session()->flash('success message', 'Item added in Cart');
+        session()->flash('success_message', 'Item added in Cart');
         return redirect()->route('product.cart');
     }
-    
+
     use WithPagination;
     public function render()
     {

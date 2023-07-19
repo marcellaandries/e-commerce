28b5f738549121463fd23ccd477e2af5135335cd
marcellaandries <marcella.andries11@gmail.com>
2023-07-19 16:07:51 +0700

    search

--- app/Http/Livewire/SearchComponent.php
+++ app/Http/Livewire/SearchComponent.php
@@ -2,12 +2,63 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Cart;
+use App\Models\Category;
 
 class SearchComponent extends Component
 {
+    public $sorting;
+    public $pagesize;
+
+    public $search;
+    public $product_cat;
+    public $product_cat_id;
+
+    public function mount()
+    {
+        $this->sorting = "default";
+        $this->pagesize = 12;
+        $this->fill(request()->only('search','product_cat','product_cat_id'));
+    }
+
+    public function store($product_id,$product_name,$product_price)
+    {
+        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+        return redirect()->route('product.cart');
+    }
+
+    use WithPagination;
     public function render()
     {
-        return view('livewire.search-component');
+        $popular_products = Product::inRandomOrder()->limit(4)->get();
+
+        if($this->sorting=='date'){
+            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id .'%')->orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+        else if($this->sorting=='price'){
+            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id .'%')->orderBy('regular_price','ASC')->paginate($this->pagesize);
+        }
+        else if($this->sorting=='price-desc'){
+            $products = Product::where('name','like','%'.$this->search .'%')->where('category_i','like','%'.$this->product_cat_id .'%')->orderBy('regular_price','DESC')->paginate($this->pagesize);
+        }
+        else{
+            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id .'%')->paginate($this->pagesize);
+        }
+        // $products = Product::paginate(12);
+        // dd($products);
+
+        $categories = Category::all();
+
+        return view('livewire.search-component',['products'=> $products, 'categories'=> $categories, 'popular_products'=> $popular_products])->layout("layouts.base");
+    }
+    public function rupiah($var_number){
+
+        $rupiah_result = "Rp " . number_format($var_number,2,',','.');
+        return $rupiah_result;
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -37,6 +38,7 @@ Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
 
+Route::get('/search', SearchComponent::class)->name('product.search');
 // Route::middleware([
 //     'auth:sanctum',
 //     config('jetstream.auth_session'),

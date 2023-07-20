d9b189448c74e8774b160c44d6d3d7dedd837012
marcellaandries <marcella.andries11@gmail.com>
2023-07-20 10:21:15 +0700

    update category

--- routes/web.php
+++ routes/web.php
@@ -82,8 +82,8 @@ Route::middleware([
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory') ;
-    // Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
-    Route::get('/admin/category/edit',AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
+    // Route::get('/admin/category/edit',AdminEditCategoryComponent::class)->name('admin.editcategory');
 
     // Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
     // Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');

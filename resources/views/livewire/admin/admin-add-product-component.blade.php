36795ceb627276277336477fe0dae0a479feb286
marcellaandries <marcella.andries11@gmail.com>
2023-07-20 11:57:07 +0700

    product add

--- resources/views/livewire/admin/admin-add-product-component.blade.php
+++ resources/views/livewire/admin/admin-add-product-component.blade.php
@@ -1,3 +1,55 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div class="container" style="padding:30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Add New Product
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{route('admin.products')}}" class="btn btn-primary pull-right">All Products</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        @if(Session::has('message'))
+                            <div class="alert alert-success" role="alert">{{Session::get('message')}}</div>
+                        @endif
+                        <form class="form-horizontal" wire:submit.prevent="storeCategory">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Product Name</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Product Name" class="form-control input-md" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Product Slug</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Product Slug" class="form-control input-md" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Short Description</label>
+                                <div class="col-md-4">
+                                    <textarea class="form-control" placeholder="Short Description"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Description</label>
+                                <div class="col-md-4">
+                                    <textarea class="form-control" placeholder="Description"></textarea>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

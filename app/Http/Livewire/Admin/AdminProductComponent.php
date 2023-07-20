7a4e23412b5d851f2168b24e3311a2870782759d
marcellaandries <marcella.andries11@gmail.com>
2023-07-20 11:16:04 +0700

    display product

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -3,11 +3,15 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Product;
 
 class AdminProductComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.admin.admin-product-component')->layout('layouts.base');
+        $products = Product::paginate(10);
+        return view('livewire.admin.admin-product-component',['products'=>$products])->layout('layouts.base');
     }
 }

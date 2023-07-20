94208d27f226234c03fe272e2194ee83f8e15c4f
marcellaandries <marcella.andries11@gmail.com>
2023-07-20 09:41:01 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 
 
 // use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
 // use App\Http\Livewire\Admin\AdminAddProductComponent;
-// use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 // use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 // use App\Http\Livewire\Admin\AdminEditProductComponent;
 // use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
@@ -82,8 +82,9 @@ Route::middleware([
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory') ;
-
     // Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/category/edit',AdminEditCategoryComponent::class)->name('admin.editcategory');
+
     // Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
     // Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
     // Route::get('/admin/product/edit/{product_slug}',AdminEditProductComponent::class)->name('admin.editproduct');

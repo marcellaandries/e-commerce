1b03b35c69db23cca447a18ba10739294e0c7257
marcellaandries <marcella.andries11@gmail.com>
2023-07-20 13:28:11 +0700

    prod form

--- resources/views/livewire/admin/admin-add-product-component.blade.php
+++ resources/views/livewire/admin/admin-add-product-component.blade.php
@@ -45,6 +45,55 @@
                                     <textarea class="form-control" placeholder="Description"></textarea>
                                 </div>
                             </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Regular Price</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Regular Price" class="form-control input-md" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Sale Price</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Sale Price" class="form-control input-md" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">SKU</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="SKU" class="form-control input-md" />
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Stock</label>
+                                <div class="col-md-4">
+                                    <select class="form-control">
+                                        <option value="instock">InStock</option>
+                                        <option value="instock">Out of Stock</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Featured</label>
+                                <div class="col-md-4">
+                                    <select class="form-control">
+                                        <option value="instock">No</option>
+                                        <option value="instock">Yes</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Quantity</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Quantity" class="form-control input-md" />
+                                </div>
+                            </div>
+
                         </form>
 
                     </div>

d8eecf5c463b6130c7baed1bc27b369463b2bc9a
marcellaandries <marcella.andries11@gmail.com>
2023-07-23 10:16:36 +0700

    route title

--- resources/views/livewire/shipping-cost-component.blade.php
+++ resources/views/livewire/shipping-cost-component.blade.php
@@ -6,7 +6,7 @@
         <div class="wrap-breadcrumb">
             <ul>
                 <li class="item-link"><a href="#" class="link">home</a></li>
-                <li class="item-link"><span>login</span></li>
+                <li class="item-link"><span>Shipping</span></li>
             </ul>
         </div>
 

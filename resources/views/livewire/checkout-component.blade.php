23c0ffe32df36f14c27fd8ea03ec2d18c2af68e4
marcellaandries <marcella.andries11@gmail.com>
2023-07-24 18:12:01 +0700

    css

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -11,111 +11,44 @@
             </ul>
         </div>
         <div class=" main-content-area">
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="wrap-address-billing">
-                        <h3 class="box-title">Billing Address</h3>
-                        {{-- <form action="#" method="get" name="frm-billing"> --}}
-                        <div class="billing-address">
-                            <p class="row-in-form">
-                                <label for="fname">first name<span>*</span></label>
-                                <input type="text" name="fname" value="" placeholder="Your name" wire:mode="firstname">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="lname">last name<span>*</span></label>
-                                <input type="text" name="lname" value="" placeholder="Your last name" wire:mode="lastname">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="email">Email Address:</label>
-                                <input type="email" name="email" value="" placeholder="Type your email" wire:mode="email">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="phone">Phone number<span>*</span></label>
-                                <input type="number" name="phone" value="" placeholder="number format" wire:mode="mobile">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="add">Address (line 1):</label>
-                                <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="line1">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="add">Address (line 2):</label>
-                                <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="line2">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="country">Country</label>
-                                <input disabled type="text" name="country" value="Indonesia" placeholder="Country" wire:mode="country">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="province">Province<span>*</span></label>
-                                <input type="text" name="province" value="" placeholder="Province" wire:mode="province">
-                                {{-- <select name="province_id" id="province_id" class="form-control">
-                                    <option value="">Select Province</option>
-                                    @foreach ($provinsi as $row)
-                                    <option value="{{$row['province_id']}}" namaprovinsi="{{$row['province']}}">{{$row['province']}}</option>
-                                    @endforeach
-                                </select> --}}
-                            </p>
-                            <p class="row-in-form">
-                                <label for="kota_id">Town / City<span>*</span></label>
-                                <input type="text" name="city" value="" placeholder="City name" wire:mode="city">
-                            </p>
-                            <p class="row-in-form">
-                                <label for="zip-code">Postcode / ZIP:</label>
-                                <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:mode="zipcode">
-                            </p>
-                            <p class="row-in-form fill-wife">
-                                {{-- <label class="checkbox-field">
-                                    <input name="create-account" id="create-account" value="forever" type="checkbox">
-                                    <span>Create an account?</span>
-                                </label> --}}
-                                <label class="checkbox-field">
-                                    <input name="different-add" id="different-add" value="1" type="checkbox" wire:model="ship_to_different">
-                                    <span>Ship to a different address?</span>
-                                </label>
-                            </p>
-                        </div>
-                        {{-- </form> --}}
-                    </div>
-                </div>
-
-                @if($ship_to_different)
+            <form>
+                <div class="row">
                     <div class="col-md-12">
                         <div class="wrap-address-billing">
-                            <h3 class="box-title">Shipping Address</h3>
+                            <h3 class="box-title">Billing Address</h3>
                             {{-- <form action="#" method="get" name="frm-billing"> --}}
                             <div class="billing-address">
                                 <p class="row-in-form">
                                     <label for="fname">first name<span>*</span></label>
-                                    <input type="text" name="fname" value="" placeholder="Your name" wire:mode="s_firstname">
+                                    <input type="text" name="fname" value="" placeholder="Your name" wire:mode="firstname">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="lname">last name<span>*</span></label>
-                                    <input type="text" name="lname" value="" placeholder="Your last name" wire:mode="s_lastname">
+                                    <input type="text" name="lname" value="" placeholder="Your last name" wire:mode="lastname">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="email">Email Address:</label>
-                                    <input type="email" name="email" value="" placeholder="Type your email" wire:mode="s_email">
+                                    <input type="email" name="email" value="" placeholder="Type your email" wire:mode="email">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="phone">Phone number<span>*</span></label>
-                                    <input type="number" name="phone" value="" placeholder="number format" wire:mode="s_mobile">
+                                    <input type="number" name="phone" value="" placeholder="number format" wire:mode="mobile">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="add">Address (line 1):</label>
-                                    <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="s_line1">
+                                    <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="line1">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="add">Address (line 2):</label>
-                                    <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="s_line2">
+                                    <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="line2">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="country">Country</label>
-                                    <input disabled type="text" name="country" value="Indonesia" placeholder="Country" wire:mode="s_country">
+                                    <input disabled type="text" name="country" value="Indonesia" placeholder="Country" wire:mode="country">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="province">Province<span>*</span></label>
-                                    <input type="text" name="province" value="" placeholder="Province" wire:mode="s_province">
+                                    <input type="text" name="province" value="" placeholder="Province" wire:mode="province">
                                     {{-- <select name="province_id" id="province_id" class="form-control">
                                         <option value="">Select Province</option>
                                         @foreach ($provinsi as $row)
@@ -125,53 +58,120 @@
                                 </p>
                                 <p class="row-in-form">
                                     <label for="kota_id">Town / City<span>*</span></label>
-                                    <input type="text" name="city" value="" placeholder="City name" wire:mode="s_city">
+                                    <input type="text" name="city" value="" placeholder="City name" wire:mode="city">
                                 </p>
                                 <p class="row-in-form">
                                     <label for="zip-code">Postcode / ZIP:</label>
-                                    <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:mode="s_zipcode">
+                                    <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:mode="zipcode">
+                                </p>
+                                <p class="row-in-form fill-wife">
+                                    {{-- <label class="checkbox-field">
+                                        <input name="create-account" id="create-account" value="forever" type="checkbox">
+                                        <span>Create an account?</span>
+                                    </label> --}}
+                                    <label class="checkbox-field">
+                                        <input name="different-add" id="different-add" value="1" type="checkbox" wire:model="ship_to_different">
+                                        <span>Ship to a different address?</span>
+                                    </label>
                                 </p>
                             </div>
                             {{-- </form> --}}
                         </div>
                     </div>
-                @endif
-            </div>
 
+                    @if($ship_to_different)
+                        <div class="col-md-12">
+                            <div class="wrap-address-billing">
+                                <h3 class="box-title">Shipping Address</h3>
+                                {{-- <form action="#" method="get" name="frm-billing"> --}}
+                                <div class="billing-address">
+                                    <p class="row-in-form">
+                                        <label for="fname">first name<span>*</span></label>
+                                        <input type="text" name="fname" value="" placeholder="Your name" wire:mode="s_firstname">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="lname">last name<span>*</span></label>
+                                        <input type="text" name="lname" value="" placeholder="Your last name" wire:mode="s_lastname">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="email">Email Address:</label>
+                                        <input type="email" name="email" value="" placeholder="Type your email" wire:mode="s_email">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="phone">Phone number<span>*</span></label>
+                                        <input type="number" name="phone" value="" placeholder="number format" wire:mode="s_mobile">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="add">Address (line 1):</label>
+                                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="s_line1">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="add">Address (line 2):</label>
+                                        <input type="text" name="add" value="" placeholder="Street at apartment number" wire:mode="s_line2">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="country">Country</label>
+                                        <input disabled type="text" name="country" value="Indonesia" placeholder="Country" wire:mode="s_country">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="province">Province<span>*</span></label>
+                                        <input type="text" name="province" value="" placeholder="Province" wire:mode="s_province">
+                                        {{-- <select name="province_id" id="province_id" class="form-control">
+                                            <option value="">Select Province</option>
+                                            @foreach ($provinsi as $row)
+                                            <option value="{{$row['province_id']}}" namaprovinsi="{{$row['province']}}">{{$row['province']}}</option>
+                                            @endforeach
+                                        </select> --}}
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="kota_id">Town / City<span>*</span></label>
+                                        <input type="text" name="city" value="" placeholder="City name" wire:mode="s_city">
+                                    </p>
+                                    <p class="row-in-form">
+                                        <label for="zip-code">Postcode / ZIP:</label>
+                                        <input type="number" name="zip-code" value="" placeholder="Your postal code" wire:mode="s_zipcode">
+                                    </p>
+                                </div>
+                                {{-- </form> --}}
+                            </div>
+                        </div>
+                    @endif
+                </div>
 
-            <div class="summary summary-checkout">
-                <div class="summary-item payment-method">
-                    <h4 class="title-box">Payment Method</h4>
-                    <p class="summary-info"><span class="title">Check / Money order</span></p>
-                    <p class="summary-info"><span class="title">Credit Cart (saved)</span></p>
-                    <div class="choose-payment-methods">
-                        <label class="payment-method">
-                            <input name="payment-method" id="payment-method-bank" value="bank" type="radio">
-                            <span>Bank Transder</span>
-                            <span class="payment-desc">BCA Account : 5270881188</span>
-                        </label>
-                        <label class="payment-method">
-                            <input name="payment-method" id="payment-method-visa" value="visa" type="radio">
-                            <span>Midtrans</span>
-                            <span class="payment-desc">Midtrans Credit Card</span>
-                        </label>
-                        {{-- <label class="payment-method">
-                            <input name="payment-method" id="payment-method-paypal" value="paypal" type="radio">
-                            <span>Paypal</span>
-                            <span class="payment-desc">You can pay with your credit</span>
-                            <span class="payment-desc">card if you don't have a paypal account</span>
-                        </label> --}}
+                <div class="summary summary-checkout">
+                    <div class="summary-item payment-method">
+                        <h4 class="title-box">Payment Method</h4>
+                        <p class="summary-info"><span class="title">Check / Money order</span></p>
+                        <p class="summary-info"><span class="title">Credit Cart (saved)</span></p>
+                        <div class="choose-payment-methods">
+                            <label class="payment-method">
+                                <input name="payment-method" id="payment-method-bank" value="bank" type="radio">
+                                <span>Bank Transder</span>
+                                <span class="payment-desc">BCA Account : 5270881188</span>
+                            </label>
+                            <label class="payment-method">
+                                <input name="payment-method" id="payment-method-visa" value="visa" type="radio">
+                                <span>Midtrans</span>
+                                <span class="payment-desc">Midtrans Credit Card</span>
+                            </label>
+                            {{-- <label class="payment-method">
+                                <input name="payment-method" id="payment-method-paypal" value="paypal" type="radio">
+                                <span>Paypal</span>
+                                <span class="payment-desc">You can pay with your credit</span>
+                                <span class="payment-desc">card if you don't have a paypal account</span>
+                            </label> --}}
+                        </div>
+                        <p class="summary-info grand-total"><span>Grand Total</span> <span class="grand-total-price">Rp 1.000.000</span></p>
+                        <button type="submit" class="btn btn-medium">Place order now</button>
                     </div>
-                    <p class="summary-info grand-total"><span>Grand Total</span> <span class="grand-total-price">Rp 1.000.000</span></p>
-                    <a href="thankyou.html" class="btn btn-medium">Place order now</a>
-                </div>
-                <div class="summary-item shipping-method">
-                    <h4 class="title-box f-title">Shipping method</h4>
-                    <p class="summary-info"><span class="title">Flat Rate</span></p>
-                    <p class="summary-info"><span class="title">Fixed Rp 500.000,00</span></p>
+                    <div class="summary-item shipping-method">
+                        <h4 class="title-box f-title">Shipping method</h4>
+                        <p class="summary-info"><span class="title">Flat Rate</span></p>
+                        <p class="summary-info"><span class="title">Fixed Rp 50.000,00</span></p>
 
+                    </div>
                 </div>
-            </div>
+            </form>
 
         </div><!--end main content area-->
     </div><!--end container-->

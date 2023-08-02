c7d6db13d85d4d28fe76d5a844d0aa78facbaf38
marcellaandries <marcella.andries11@gmail.com>
2023-08-02 16:58:52 +0700

    Add Edit Address Comp

---
+++ app/Http/Livewire/AddAddressComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddAddressComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-address-component');
+    }
+}
---
+++ app/Http/Livewire/EditAddressComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EditAddressComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.edit-address-component');
+    }
+}

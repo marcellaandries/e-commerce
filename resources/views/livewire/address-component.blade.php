7e52015614f6439d9f4593301ff04b0e951e032d
marcellaandries <marcella.andries11@gmail.com>
2023-08-02 14:01:29 +0700

    css

--- resources/views/livewire/address-component.blade.php
+++ resources/views/livewire/address-component.blade.php
@@ -37,84 +37,62 @@
                         @foreach ($addresses as $address)
                             <section style="background-color: #eee;">
                                 <div class="container py-5 mb-8">
-                                {{-- <div class="row justify-content-center mb-3"> --}}
-                                    {{-- <div class="col-md-12 col-xl-10"> --}}
-                                    {{-- <div class="card shadow-0 border rounded-3"> --}}
-                                        {{-- <div class="card-body"> --}}
 
+                                    <div class="row">
+                                        <div class="pl-35 col-md-1 col-lg-1 col-xl-1 mb-1 mb-lg-1 mt-2">
+                                            <span class="mb-1 me-1 text-primary">
+                                                @if($address->priority)
+                                                    <input type="radio" id="isPriority" name="isPriority" value="1" checked=checked>
 
-                                            <div class="row">
-                                                {{-- <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
-                                                <div class="bg-image hover-zoom ripple rounded ripple-surface">
-                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(4).webp"
-                                                    class="w-100" />
-                                                    <a href="#!">
-                                                    <div class="hover-overlay">
-                                                        <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
-                                                    </div>
-                                                    </a>
-                                                </div>
-                                                </div> --}}
-                                                <div class="col-md-18 col-lg-9 col-xl-9 pl-25">
-                                                        <h5>{{$address->firstname}}</h5>
-                                                        <div class="d-flex flex-row">
-                                                            <div class="text-primary mb-1 me-2">
-                                                                {{$address->line1}}
-                                                            </div>
-                                                            {{-- <span>310</span> --}}
-                                                        </div>
-                                                        <div class="mt-1 mb-0 text-muted small">
-                                                            <span>{{$address->province}}</span>
-                                                            <span class="text-primary"> • </span>
-                                                            <span>{{$address->city}}</span>
-                                                            <span class="text-primary"> • </span>
-                                                            <span>{{$address->zipcode}}<br /></span>
-                                                        </div>
-                                                        {{-- <div class="mb-2 text-muted small">
-                                                            <span>Unique design</span>
-                                                            <span class="text-primary"> • </span>
-                                                            <span>For men</span>
-                                                            <span class="text-primary"> • </span>
-                                                            <span>Casual<br /></span>
-                                                        </div> --}}
-                                                        <p class="text-truncate mb-4 mb-md-0">
-                                                            {{$address->mobile}}
-                                                        </p>
-                                                </div>
-                                                <div class="col-md-6 col-lg-3 col-xl-3 pl-25 border-sm-start-none border-start">
-                                                <div class="d-flex flex-row align-items-center mb-1">
-                                                    <h5 class="mb-1 me-1 text-primary">
-                                                        @if($address->priority)
-                                                            <input type="radio" id="isPriority" name="isPriority" value="1" checked=checked>
-                                                            <span class="label label-primary">Primary</span>
-                                                        @else
-                                                            <input type="radio" id="isPriority" name="isPriority" value="0">
-                                                            <span class="label label-default">Not Primary</span>
-                                                        @endif
-                                                        {{-- {{$address->priority}} --}}
-                                                    </h5>
-                                                    {{-- <span class="text-danger"><s>$20.99</s></span> --}}
-                                                </div>
-                                                <h6 class="text-primary">{{$address->label}}</h6>
-                                                <div class="d-flex flex-column mt-4">
+                                                @else
+                                                    <input type="radio" id="isPriority" name="isPriority" value="0">
 
-                                                        {{-- <button class="btn btn-primary btn-sm" type="button">Select</button>
-                                                        <button class="btn btn-primary btn-sm mt-2" type="button">
-                                                        Edit
-                                                        </button> --}}
+                                                @endif
+                                            </span>
 
-                                                        <a href="#"><i class="fa fa-check-square-o fa-2x text-info" title="choose"></i></a>
-                                                        <a href="#"><i class="ml-8 fa fa-edit fa-2x text-info ml-12" title="edit"></i></a>
-                                                        <a href="#" onclick="confirm('Are you sure, you want to delete this product?') || event.stopImmediatePropagation()" style="margin-left:10px;" wire:click.prevent="deleteAddress({{$address->id}})"><i class="fa fa-times fa-2x text-danger" title="delete"></i></a>
-                                                </div>
+                                        </div>
+                                        <div class="col-md-2 col-lg-2 col-xl-2 pl-25 mt-08">
+                                            <h5>{{$address->firstname}}</h5>
+                                            <p class="text-primary text-truncate mb-4 mb-md-0">
+                                                {{$address->mobile}}
+                                            </p>
+                                        </div>
+                                        <div class="col-md-5 col-lg-5 col-xl-5 pl-25 mt-2">
+                                            <div class="d-flex flex-row">
+                                                <div class="text-primary mb-1 me-2">
+                                                    {{$address->line1}}
                                                 </div>
                                             </div>
+                                            <div class="mt-1 mb-0 text-muted small">
+                                                <span>{{$address->province}}</span>
+                                                <span class="text-primary"> • </span>
+                                                <span>{{$address->city}}</span>
+                                                <span class="text-primary"> • </span>
+                                                <span>{{$address->zipcode}}<br /></span>
+                                            </div>
+                                        </div>
 
+                                        <div class="col-md-2 col-lg-2 col-xl-2 border-sm-start-none border-start mt-04 pl-35">
+                                            <div class="d-flex flex-row align-items-center mb-1">
+                                                <h6 class="mb-1 me-1 text-primary">
+                                                    @if($address->priority)
+                                                        <span class="label label-primary">Primary</span>
+                                                    @else
+                                                        <span class="label label-default">Not Primary</span>
+                                                    @endif
+                                                </h6>
+                                            </div>
+                                            <h6 class="label label-info mb-1">{{$address->label}}</h6>
+                                        </div>
 
-                                        {{-- </div> --}}
-                                    {{-- </div> --}}
-                                    {{-- </div> --}}
-                                {{-- </div> --}}
+                                        <div class="col-md-2 col-lg-2 col-xl-2 border-sm-start-none border-start mt-2">
+                                            <div class="d-flex flex-column mt-4 ">
+                                                <a href="#"><i class="fa fa-edit fa-2x text-info ml-12" title="edit"></i>&nbsp;Edit</a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
                                 </div>
                             </section>
                             <hr>

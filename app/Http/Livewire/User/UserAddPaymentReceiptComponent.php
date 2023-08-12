8ef69b5b5b11f95ee53aa4889971fc75cd27ccad
marcellaandries <marcella.andries11@gmail.com>
2023-08-12 16:48:52 +0700

    user/UserAddPaymentReceiptComponent

---
+++ app/Http/Livewire/User/UserAddPaymentReceiptComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class UserAddPaymentReceiptComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.user-add-payment-receipt-component');
+    }
+}

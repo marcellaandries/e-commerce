156e8dd7bf6a76e89caf570901078cd087604547
marcellaandries <marcella.andries11@gmail.com>
2023-08-13 16:10:06 +0700

    add wire model

--- app/Http/Livewire/User/UserAddPaymentReceiptComponent.php
+++ app/Http/Livewire/User/UserAddPaymentReceiptComponent.php
@@ -4,8 +4,21 @@ namespace App\Http\Livewire\User;
 
 use Livewire\Component;
 
+use App\Models\PaymentReceipt;
+use App\Models\Transaction;
+use App\Models\Order;
+
+use Illuminate\Support\Str;
+use Livewire\WithFileUploads;
+use Carbon\Carbon;
+
 class UserAddPaymentReceiptComponent extends Component
 {
+    use WithFileUploads;
+    public $sender_name;
+    public $transfer_date;
+    public $paid_amount;
+    public $payment_receipt;
     public function render()
     {
         return view('livewire.user.user-add-payment-receipt-component')->layout('layouts.base');
